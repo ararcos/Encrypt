1febc6e1e23a631958bea315db9caa334cd0e39a
Alexander <alexanderarcos66@gmail.com>
2020-01-09 22:12:33 -0500

    Correcion transposición
    Se lleno la matriz con # cuando no se completa

--- encriptar/src/encriptar/encriptar.java
+++ encriptar/src/encriptar/encriptar.java
@@ -7,33 +7,38 @@ public class encriptar {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String mensaje,mensajeEncriptado;
-		
+		int tamaño;
 		System.out.println("Ingrese el mensaje que desea encriptar:");
 		mensaje=sc.nextLine();
-		mensajeEncriptado=encriptarTransposicion(mensaje);
-		mensajeEncriptado=encriptarSustitucion(mensaje);
+		System.out.println("Ingrese el tamaño de la matriz");
+		tamaño=sc.nextInt();
+		mensajeEncriptado=encriptarTransposicion(mensaje,tamaño);
+		mensajeEncriptado=encriptarSustitucion(mensajeEncriptado);
 		System.out.println(mensajeEncriptado);
 	}
-	public static String encriptarTransposicion(String mensaje) {
+	public static String encriptarTransposicion(String mensaje,int tamaño) {
 		char[] mensajeChar =mensaje.toCharArray();
-		int filasMensaje= (mensajeChar.length/6)+1;
-		char metodoEncriptar1 [][] = new char[filasMensaje][6];
-		int desplazar=0;
-		for (int i = 0,j=0,k=0; i < mensajeChar.length; i++,k++) {
-			if (k==6) {
-				k=0;
-				j++;
+		int filasMensaje= (mensajeChar.length/tamaño)+1;
+		char metodoEncriptar1 [][] = new char[filasMensaje][tamaño];
+		int desplazar=0,k=0;
+		for (int i = 0; i < filasMensaje; i++) {
+			for (int j = 0; j < tamaño; j++) {
+				if (k<mensajeChar.length) {
+					metodoEncriptar1[i][j]=mensajeChar[k];
+					k++;
+				}else {
+					metodoEncriptar1[i][j]='#';
+				}
 			}
-			metodoEncriptar1[j][k]=mensajeChar[i];
 		}
 		
 		String mensajeEncriptado="";
-		for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < tamaño; i++) {
 			for (int j = 0; j < filasMensaje; j++) {
 				int dato = metodoEncriptar1[j][i];
 				if (dato==32) {
 					mensajeEncriptado+='$';
-				}else if (dato!=0) {
+				}else {
 					mensajeEncriptado+=metodoEncriptar1[j][i];
 				}
 			}
@@ -42,8 +47,8 @@ public class encriptar {
 	}
 	public static String encriptarSustitucion(String mensaje) {
 		String mensajeEncriptado="";
-		String abecedario = "abcdefghijklmnñopqrstuvwxyz$ ";
-		String abecedarioEncriptado="tuvenwfoyizlxhspmjrñcagkbqd&&";
+		String abecedario = "abcdefghijklmnñopqrstuvwxyz$#";
+		String abecedarioEncriptado="tuvenwfoyizlxhspmjrñcagkbqd&$";
 		for (int i = 0; i < mensaje.length(); i++) {
 			char letra=mensaje.charAt(i);
 			int posicion= abecedario.indexOf(letra);
